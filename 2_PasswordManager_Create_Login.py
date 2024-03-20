f6697c348625b649f43257ef00ff27a4f81988a6
mariuspetrica <123092005+mariuspetrica@users.noreply.github.com>
2024-03-20 13:30:23 +0200

    Add files via upload

--- 2_PasswordManager_Create_Login.py
+++ 2_PasswordManager_Create_Login.py
@@ -6,15 +6,15 @@ Manager={}
 def create():
     username=input("Enter username: ")
     password=getpass.getpass("Enter password: ")
-    hashed_pass=hashlib.sha256(password.encode()).hexdigest
+    hashed_pass=hashlib.sha256(password.encode()).hexdigest()
     Manager[username]=hashed_pass
     print("Succesfully saved!")
 
 def login():
     username=input("Enter username: ")
     password=getpass.getpass("Enter password: ")
-    hashed_pass=hashlib.sha256(password.encode()).hexdigest
-    if username in  Manager.keys and Manager[username] == hashed_pass:
+    hashed_pass=hashlib.sha256(password.encode()).hexdigest()
+    if username in  Manager.keys() and Manager[username] == hashed_pass:
         print("Login succesfull! ")
     else:
         print("Login failed! Try again!")
@@ -22,13 +22,20 @@ def login():
 
 def main():
     while True:
-        choice = input("Enter 1 to create account, 2 to login or 0 to exit: ")
-        if(choice)==1:
+        choice = input("Enter 1 to create account, 2 to login, 3 to see all users or 0 to exit: ")
+        if choice =="1":
             create()
-        elif choice==2:
+        elif choice=="2":
             login()
-        elif choice==0:
+        elif choice == "3":
+            for username in Manager:
+                hashed_pass=(Manager[username])
+                print (f"Username is {username}")
+                print(f" and password is {hashed_pass}")
+        elif choice=="0":
                 break
         else: print("Invalid choice!")
 
+if __name__== "__main__":
+    main()
 
